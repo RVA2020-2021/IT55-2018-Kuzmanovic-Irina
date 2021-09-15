a1028de39c58a76ddc3836f3e33907efc54a91fa
IrinaKuzmanovic <irinakuzmanovic@gmail.com>
2021-09-16 01:13:42 +0200

    redirects

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,5 +1,14 @@
-export const RACUN_URL='http://localhost:8083/racun';
+/*export const RACUN_URL='http://localhost:8083/racun';
 export const PROIZVODJAC_URL='http://localhost:8083/proizvodjac';
 export const PROIZVOD_URL='http://localhost:8083/proizvod';
 export const STAVKA_RACUNA_URL='http://localhost:8083/stavkaRacuna';
 export const STAVKE_ZA_RACUN_URL='http://localhost:8083/stavkeZaRacuneId';
+*/
+
+export const RACUN_URL = 'https://backend-2021.herokuapp.com/racun';
+export const PROIZVODJAC_URL = 'https://backend-2021.herokuapp.com/proizvodjac';
+export const PROIZVOD_URL = 'https://backend-2021.herokuapp.com/proizvod';
+export const STAVKA_RACUNA_URL =
+  'https://backend-2021.herokuapp.com/stavkaRacuna';
+export const STAVKE_ZA_RACUN_URL =
+  'https://backend-2021.herokuapp.com/stavkeZaRacuneId';
